1dd3fd97bc56d15be60585f2a0bf3393b7114b4a
shefain <muhammadShefain123@gmail.com>
2025-04-26 15:08:45 +0600

    first commit of header some unfinished workds

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,18 @@
 export default {
     plugins: {
-      "@tailwindcss/postcss": {},
+      "@tailwindcss/postcss": {
+        theme:{
+          extends:{
+            conatainer:{
+              center: true,
+              padding: '1.5rem',
+            },
+            color:{
+              primary:"#00d991"
+            }
+          }
+        }
+      },
     }
   }
   
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-center font-semibold "> Welcome to React </h1>
+      <Header />
     </>
   );
 }
