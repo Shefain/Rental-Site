a8debdffc439438b39d0272fa00ce1bb7e9fe0ce
shefain <muhammadShefain123@gmail.com>
2025-04-27 15:16:30 +0600

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
-  return ( 
+  return (
     <>
       <Header />
-      
+      <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <Sidebar />
+      </div>
     </>
-   );
-}
- 
+  );
+};
+
 export default App;
